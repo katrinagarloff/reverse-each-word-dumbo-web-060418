abb0f306725a881e20cecb3bed8c435802603a9a
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-18 16:41:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word(str)
   str_ar = str.split()
-  reverse_ar = []
-  str_ar.each do |word|
-    reverse_ar.push(word.reverse)
+  str_ar.collect do |word|
+    word.reverse
   end
-  return reverse_ar.join(" ")
 end
 
 puts reverse_each_word("meow, mix")
